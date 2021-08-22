87054a3343f4e030090d0d30ccc41a2743fb14e0
DominicTylor <dominictylor13@gmail.com>
2021-08-22 16:57:19 +0300

    [LCP-002] Create package flow action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,51 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import { getInput, setFailed, info, setOutput } from '@actions/core';
+import { context } from '@actions/github'
+import { findPackageJson, extract } from './util';
+
+function checkStableVersionBranch(): boolean {
+  return context.ref === 'refs/heads/master';
+}
+
+function createStablePackage(version: string): string {
+  if (/^(\d+.\d+.\d+)$/.test(version)) {
+    info('Hotfix package flow');
+
+    return version.replace(/^(\d+.\d+.)(\d+)$/, (_, start, patch) => start + (parseInt(patch) + 1));
+  }
+
+  info('Stable package flow');
+
+  return version.replace(/-rc.\d+$/, '');
+}
+
+function createReleaseCandidatePackage(version: string): string {
+  if (/^(\d+.\d+.\d+)$/.test(version)) {
+    info('RC package flow, first increment');
+
+    return version.replace(/^(\d+.)(\d+)/, (_, start, minor) => start + (parseInt(minor) + 1) + '.0-rc.0');
+  }
+
+  info('RC package flow, not first increment');
+
+  return version.replace(/^(\d+.\d+.\d+-rc.)(\d+)$/, (_, start, release) => start + (parseInt(release) + 1));
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const followSymbolicLinks: boolean = getInput('follow-symlinks').toLowerCase() !== 'false';
+    const path: string = getInput('path') ? process.env.GITHUB_WORKSPACE + '/' + getInput('path') : await findPackageJson(followSymbolicLinks);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const packageVersion: string = await extract(path);
+    info('Old version' + packageVersion);
 
-    core.setOutput('time', new Date().toTimeString())
+    const newPackageVersion = checkStableVersionBranch()
+        ? createStablePackage(packageVersion) : createReleaseCandidatePackage(packageVersion);
+    info('New version' + newPackageVersion);
+
+    setOutput('next-version', newPackageVersion);
   } catch (error) {
-    core.setFailed(error.message)
+    setFailed(error.message);
   }
 }
 
-run()
+run();
---
+++ src/util.ts
@@ -0,0 +1,26 @@
+import { create } from '@actions/glob';
+
+interface PackageFile {
+  version: string
+}
+
+const patterns = ['**/package.json', '!**/node_modules/'].join('\n');
+
+export async function findPackageJson(followSymlinks: boolean): Promise<string> {
+  const globber = await create(patterns, { followSymbolicLinks: followSymlinks });
+  const files = await globber.glob();
+
+  return files[0];
+}
+
+export async function extract(path: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    try {
+      const packageFile: PackageFile = require(path);
+
+      return resolve(packageFile.version);
+    } catch {
+      return reject(new Error('Invalid package.json format or path'));
+    }
+  });
+}
