e183f56c003158513e23312070aa0f5413adf543
DominicTylor <dominictylor13@gmail.com>
2021-08-23 21:34:18 +0300

    [LCP-002] Build 0.0.2

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,64 @@
-import { getInput, setFailed, info, setOutput } from '@actions/core';
-import { context } from '@actions/github'
-import { findPackageJson, extract } from './util';
+import {extract, findPackageJson} from './util'
+import {getInput, info, setFailed, setOutput} from '@actions/core'
+import {context} from '@actions/github'
 
 function checkStableVersionBranch(): boolean {
-  return context.ref === 'refs/heads/master';
+  return context.ref === 'refs/heads/master'
 }
 
 function createStablePackage(version: string): string {
   if (/^(\d+.\d+.\d+)$/.test(version)) {
-    info('Hotfix package flow');
+    info('Hotfix package flow')
 
-    return version.replace(/^(\d+.\d+.)(\d+)$/, (_, start, patch) => start + (parseInt(patch) + 1));
+    return version.replace(
+      /^(\d+.\d+.)(\d+)$/,
+      (_, start, patch) => start + (parseInt(patch) + 1)
+    )
   }
 
-  info('Stable package flow');
+  info('Stable package flow')
 
-  return version.replace(/-rc.\d+$/, '');
+  return version.replace(/-rc.\d+$/, '')
 }
 
 function createReleaseCandidatePackage(version: string): string {
   if (/^(\d+.\d+.\d+)$/.test(version)) {
-    info('RC package flow, first increment');
+    info('RC package flow, first increment')
 
-    return version.replace(/^(\d+.)(\d+)/, (_, start, minor) => start + (parseInt(minor) + 1) + '.0-rc.0');
+    return version.replace(
+      /^(\d+.)(\d+)/,
+      (_, start, minor) => `${start + (parseInt(minor) + 1)}.0-rc.0`
+    )
   }
 
-  info('RC package flow, not first increment');
+  info('RC package flow, not first increment')
 
-  return version.replace(/^(\d+.\d+.\d+-rc.)(\d+)$/, (_, start, release) => start + (parseInt(release) + 1));
+  return version.replace(
+    /^(\d+.\d+.\d+-rc.)(\d+)$/,
+    (_, start, release) => start + (parseInt(release) + 1)
+  )
 }
 
 async function run(): Promise<void> {
   try {
-    const followSymbolicLinks: boolean = getInput('follow-symlinks').toLowerCase() !== 'false';
-    const path: string = getInput('path') ? process.env.GITHUB_WORKSPACE + '/' + getInput('path') : await findPackageJson(followSymbolicLinks);
+    const followSymbolicLinks: boolean =
+      getInput('follow-symlinks').toLowerCase() !== 'false'
+    const path: string = getInput('path')
+      ? `${process.env.GITHUB_WORKSPACE}/${getInput('path')}`
+      : await findPackageJson(followSymbolicLinks)
 
-    const packageVersion: string = await extract(path);
-    info('Old version' + packageVersion);
+    const packageVersion: string = await extract(path)
+    info(`Old version${packageVersion}`)
 
     const newPackageVersion = checkStableVersionBranch()
-        ? createStablePackage(packageVersion) : createReleaseCandidatePackage(packageVersion);
-    info('New version' + newPackageVersion);
+      ? createStablePackage(packageVersion)
+      : createReleaseCandidatePackage(packageVersion)
+    info(`New version${newPackageVersion}`)
 
-    setOutput('next-version', newPackageVersion);
+    setOutput('next-version', newPackageVersion)
   } catch (error) {
-    setFailed(error.message);
+    setFailed(error.message)
   }
 }
 
-run();
+run()
--- src/util.ts
+++ src/util.ts
@@ -1,26 +1,28 @@
-import { create } from '@actions/glob';
+import {create} from '@actions/glob'
 
 interface PackageFile {
   version: string
 }
 
-const patterns = ['**/package.json', '!**/node_modules/'].join('\n');
+const patterns = ['**/package.json', '!**/node_modules/'].join('\n')
 
-export async function findPackageJson(followSymlinks: boolean): Promise<string> {
-  const globber = await create(patterns, { followSymbolicLinks: followSymlinks });
-  const files = await globber.glob();
+export async function findPackageJson(
+  followSymlinks: boolean
+): Promise<string> {
+  const globber = await create(patterns, {followSymbolicLinks: followSymlinks})
+  const files = await globber.glob()
 
-  return files[0];
+  return files[0]
 }
 
 export async function extract(path: string): Promise<string> {
   return new Promise((resolve, reject) => {
     try {
-      const packageFile: PackageFile = require(path);
+      const packageFile: PackageFile = require(path)
 
-      return resolve(packageFile.version);
+      return resolve(packageFile.version)
     } catch {
-      return reject(new Error('Invalid package.json format or path'));
+      return reject(new Error('Invalid package.json format or path'))
     }
-  });
+  })
 }
